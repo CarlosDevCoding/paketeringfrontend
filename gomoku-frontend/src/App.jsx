ae83452ab140f4e8ed3618fb49c03fa0436c856c
Julia Taiba <j.taiba.97@hotmail.com>
2025-09-01 10:57:16 +0200

    Added react-router structure.

--- gomoku-frontend/src/App.jsx
+++ gomoku-frontend/src/App.jsx
@@ -1,10 +1,15 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router";
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" /*element={X}*/ />
+          <Route path="/game" /*element={X}*/ />
+        </Routes>
+      </Router>
     </>
   )
 }
