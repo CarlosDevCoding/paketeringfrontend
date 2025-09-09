efda7908275115199e48ccd512eacfba0e6deabe
ilyafredriksson <fredrikssonilya@gmail.com>
2025-09-09 10:51:51 +0200

    Cell komponent

---
+++ gomoku-frontend/src/Components/Cell/Cell.jsx
@@ -0,0 +1,10 @@
+import './Cell.css';
+
+
+export default function Cell({ value, onClick }) {
+  return (
+    <div className={`cell ${value || 'empty'}`} onClick={onClick}>
+      {value}
+    </div>
+  );
+}
--- gomoku-frontend/src/Components/GameComponent/GameComponent.jsx
+++ gomoku-frontend/src/Components/GameComponent/GameComponent.jsx
@@ -1,18 +1,18 @@
+// src/Components/GameComponent/GameComponent.jsx
 import './GameBoardComponent.css';
+import Cell from '../Cell/Cell.jsx'; //  import av Cell
 
-export default function GameComponent ({ board, onCellClick }) {
+export default function GameComponent({ board, onCellClick }) {
   return (
     <div className="board">
       {board.map((row, rowIndex) => (
         <div key={rowIndex} className="row">
           {row.map((cell, colIndex) => (
-            <div 
+            <Cell
               key={`${rowIndex}-${colIndex}`}
-              className={`cell ${cell || 'empty'}`}
+              value={cell}
               onClick={() => onCellClick(rowIndex, colIndex)}
-            >
-              {cell}
-            </div>
+            />
           ))}
         </div>
       ))}
